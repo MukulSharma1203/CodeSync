2513c15d968dffb7acecc66378a788bc09952fe3
Mukul Sharma <mukulsharma.1203@gmail.com>
2026-06-06 23:20:39 +0530

    database initialize

---
+++ Backend/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+const connectDB = async ()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+
+        console.log(connectionInstance.connection.host);
+    } catch (error) {
+        console.log("Error in connecting to Database : " , error);
+    }
+}
+
+connectDB();
