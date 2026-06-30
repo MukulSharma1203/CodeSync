a9ee25a84427230822e038521ff2f66eed69b043
Mukul Sharma <mukulsharma.1203@gmail.com>
2026-06-30 13:48:25 +0530

    .

--- Backend/src/constants.js
+++ Backend/src/constants.js
@@ -1,15 +1,17 @@
 export const DB_NAME = "mukul_1203";
 
 export const accessOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 1 * 24 * 60 * 60 * 1000 // 1 day
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite:
+    process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 1 * 24 * 60 * 60 * 1000,
 };
 
 export const refreshOptions = {
-    secure: process.env.NODE_ENV === "production",
-    httpOnly: true,
-    sameSite: "strict",
-    maxAge: 10 * 24 * 60 * 60 * 1000 // 10 days
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite:
+    process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 10 * 24 * 60 * 60 * 1000,
 };
