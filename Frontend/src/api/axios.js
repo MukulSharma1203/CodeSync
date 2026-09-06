038e58faa34fe333f0d1f008105b55e47f373f28
MukulSharma1203 <mukulsharma.1203@gmail.com>
2026-09-06 19:38:02 +0530

    login fix

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+    baseURL: import.meta.env.PROD
+        ? "/api"
+        : import.meta.env.VITE_BACKEND_URL,
     withCredentials: true,
 });
 
